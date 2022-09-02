14b526f952ac091af75f60019184e00ee255f9d0
kamran <khosravi42@gmail.com>
2022-09-02 17:30:14 -0500

    mongoose added

--- index.js
+++ index.js
@@ -2,10 +2,31 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-/* Mongo calling */
-const run = require("./mongodb");
+/* Mongoose calling */
+const mongoose = require("mongoose");
 
-run().catch(console.dir);
+const test = require("./mongoose");
+const uri = "mongodb://localhost:27017/test";
+mongoose.connect(uri).then(
+  () => {
+    console.log("Mongoose is connected");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+/* Mongoose Schema */
+const test1 = new test({ name: "modulized" });
+test.findOne({ name: "modulized" }, function (err, doc) {
+  if (err) return console.log(err);
+  if (doc) return console.log(`${doc.name} already exists`);
+
+  test1.save((err) => {
+    if (err) return res.status(500).send(err);
+    return console.log("new documnet has been added");
+  });
+});
 
 /* Insert a sample document */
 /* Create a new MongoClient */
